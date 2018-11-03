1566367a521a10a1f426f4d0b030a98f1cab7952
saurabh gaur <gaursaurabh68@gmail.com>
2018-11-03 20:11:27 +0530

    create react boilerplate

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../assets/scss/main.scss';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="header">
+        <h1 className="header__title">My React App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
